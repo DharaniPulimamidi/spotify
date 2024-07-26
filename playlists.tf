d34d1648178142661acfeb0d035531fcf3e0a0eb
DharaniPulimamidi <dxp53770@ucmo.edu>
2024-07-26 12:07:52 -0500

    Initial commit

--- playlists.tf
+++ playlists.tf
@@ -1,4 +1,18 @@
 resource "spotify_playlist" "Bollywood" {
     name = "Bollywood"
-    tracks = ["131yybV7A3TmC34a0qE8u8"]
+    tracks = ["6Ee9j3KfgdNTlXcK2XSpH6"]
+}
+
+data "spotify_search_track" "Eminem" {
+    artist = "Eminem"
+  
+}
+
+resource "spotify_playlist" "slimShady" {
+    name = "Slim Shady"
+    tracks = [data.spotify_search_track.Eminem.tracks[0].id,
+    data.spotify_search_track.Eminem.tracks[1].id,
+    data.spotify_search_track.Eminem.tracks[2].id]
+
+  
 }
